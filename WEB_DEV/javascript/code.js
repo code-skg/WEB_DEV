a5967cf6894c628d608ad570699b3ee7b80dbda8
code-skg <sattuamar123@gmail.com>
2025-02-23 16:25:51 +0530

    Update JavaScript code to demonstrate array reference behavior and add temporary runner file

--- WEB_DEV/javascript/code.js
+++ WEB_DEV/javascript/code.js
@@ -1,8 +1,8 @@
-
-
-// for(let i = 0 ; i < 5 ; i++){
-//     console.log(i);
-// }
-for(let i = 0 ; i < 10 ; i++){
-    console.log(i+1);
-}
+let arr1 = [1,2,3,4];
+let arr2 = [1,2,3,4];
+let arr3 = arr1; 
+arr1.push(3);
+console.log(arr1);//prints [1,2,3,4,3]
+console.log(arr3);// also prints [1,2,3,4,3]
+// beacuse both are refrencing the same array here ; 
+//so changes reflecs in both the arrat; 
