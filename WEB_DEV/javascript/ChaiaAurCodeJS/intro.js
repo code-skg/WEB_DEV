91f2d0a2093b8c88ae0f930875f33e63469aeea1
code-skg <sattuamar123@gmail.com>
2025-04-08 11:25:15 +0530

    Add intro.js and note.js files with initial content on variable declaration and hoisting in JavaScript

---
+++ WEB_DEV/javascript/ChaiaAurCodeJS/intro.js
@@ -0,0 +1,5 @@
+// var a ; 
+// console.log(a);
+const a = 10 ;
+console.log(a);
+a = 10 ; 
---
+++ WEB_DEV/javascript/ChaiaAurCodeJS/note.js
@@ -0,0 +1,22 @@
+/*Hoisting is JavaScript's default behavior of moving 
+declarations (not initializations) to the top of 
+their scope (either global or function scope) before 
+code is executed.
+
+In simple terms:
+
+JavaScript "hoists" (lifts) the variable and function 
+declarations to the top of their scope during the compile phase.
+
+Variable Hoisting Example
+
+
+console.log(a); // undefined
+var a = 5;
+What JavaScript really sees behind the scenes:
+
+
+var a;        // declaration is hoisted
+console.log(a); // undefined
+a = 5;        // initialization happens here
+🔸 var gets hoisted and initialized with undefined.*/
