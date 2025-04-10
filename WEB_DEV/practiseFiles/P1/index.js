e2954beb0274b9067ffdc4c46aedb1b97310dcaf
code-skg <sattuamar123@gmail.com>
2025-04-10 18:55:29 +0530

    Refactor code structure for improved readability and maintainability

--- WEB_DEV/practiseFiles/P1/index.js
+++ WEB_DEV/practiseFiles/P1/index.js
@@ -25,19 +25,65 @@
 // void greet (string word){
 //     cout<<"hello "<<word<<endl; 
 // }
-function fun1(word){
-    console.error(` say hi1 to ${word} !`);
-}
-//fuction expression
-let fun2 = function (word){
-    console.error(` say hi2 to ${word} !`);
-} 
+// function fun1(word){
+//     console.error(` say hi1 to ${word} !`);
+// }
+// //fuction expression
+// let fun2 = function (word){
+//     console.error(` say hi2 to ${word} !`);
+// } 
 
-// arrow funtion 
-let fun3 = (word) => {
-    console.error(` say hi3 to ${word} !`);
-}
+// // arrow funtion 
+// let fun3 =  (word) => {
+//     console.error(` say hi3 to ${word} !`);
+// }
 
-fun1("Satyam");
-fun2("Satyam");
-fun3("Satyam");
+// fun1("Satyam");
+// fun2("Satyam");
+// fun3("Satyam");
+
+
+
+
+// let area = (length , breadth) => {
+//     console.log("area of rectangle =" , length*breadth);
+    
+// }
+// area(5,3);
+
+// const check = (num) => {
+//     if(num%2===0){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+// let toCheck = (num) => (num % 2 === 0) ? true : false;
+// console.log(toCheck(4));
+
+
+
+// function stringRevesal (str){
+//     return str.split("").reverse().join(""); 
+// }
+// console.log(stringRevesal("JavaScript"));
+
+function additon (a , b ){
+    return a+b;
+}
+function subtraction  (a , b ){
+    return a-b;
+}
+function multiplication (a , b ){
+    return a*b;
+}
+function division (a , b ){
+    return a/b;
+}
+let calculate = (a , b , callback) => {
+    return callback(a, b);
+}
+console.log(calculate (5,3 , multiplication));
+
+ 
