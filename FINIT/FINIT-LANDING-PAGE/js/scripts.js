fefdb62c04611777433cdf3df42ee8d422b952a1
code-skg <sattuamar123@gmail.com>
2025-04-12 01:29:21 +0530

    home page complted 1 page

--- FINIT/FINIT-LANDING-PAGE/js/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const learnMoreBtn = document.getElementById('learnMoreBtn');
-    
-    learnMoreBtn.addEventListener('click', () => {
-      alert('Welcome to the Finance Society! Explore our events and resources.');
-    });
-  });
-  
